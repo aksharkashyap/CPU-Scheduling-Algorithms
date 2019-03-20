68e111152a7823a30c4d8e283a9879ce2d4338ed
aksharkashyap <48638602+aksharkashyap@users.noreply.github.com>
2019-03-20 11:17:13 +0530

    Add files via upload

--- mysrtf.c
+++ mysrtf.c
@@ -5,8 +5,8 @@
 
 int main(){
 
-int arrivalTime[50],burstTime[50],completionTime,waitingTime,TurnAroundTime;
-int smallestBT=0;
+int arrivalTime[50],burstTime[50],completionTime[50],waitingTime[50],TurnAroundTime[50];
+int smallestBT=0, totalBT=0,pos;
 
 int n;
 printf("Please enter number of processes: ");
@@ -18,19 +18,45 @@ scanf("%d",&arrivalTime[i]);
 
 printf("Burst Time for Process: %d\n",i+1);
 scanf("%d",&burstTime[i]);
+
+totalBT += burstTime[i];
 }
 
-for(int i=0; i<n;i++){
-    if(burstTime[i] < burstTime[i+1]){
-        smallestBT = burstTime[i];
-    }
-    else if(burstTime[i+1] < burstTime[i]){
-        smallestBT = burstTime[i+1];
-    }
+while(totalBT !=0){
 
-    }
+for (int time=0;;time++){
 
-printf(" smallest burstTime %d ",smallestBT);
+    for(int i=0;i<n;i++){
+
+        if(arrivalTime[i]<=time){
+
+            for (int j=i;j<n;j++){
+                if(burstTime[j] < burstTime[j+1]){
+                    smallestBT = burstTime[j];
+                    pos = j;
+                }
+                else{
+                    smallestBT = burstTime[j+1];
+                    pos = j+1;
+                }
+            }
+            burstTime[pos]--;
+            completionTime[pos]++;
+
+        }
+    }
+}
+}
+
+for (int i=0; i<n; i++){
+    TurnAroundTime[i] = completionTime[i] - arrivalTime[i];
+    waitingTime[i] = TurnAroundTime[i] - burstTime[i];
+}
+
+for (int i=0; i<n; i++){
+    printf("%d\n",TurnAroundTime[i] );
+    printf("%d\n",waitingTime[i] );
+}
 
 
 }
